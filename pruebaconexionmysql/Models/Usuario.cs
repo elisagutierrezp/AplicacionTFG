44d4b69bc4f1a8a2f260d97b664546b4c81281e7
laura-bp2399 <laura.bujalance@edu.uah.es>
2021-03-29 11:23:23 +0200

    migración a 5.0 e inicio de login

--- pruebaconexionmysql/Models/Usuario.cs
+++ pruebaconexionmysql/Models/Usuario.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 
 #nullable disable
 
 namespace pruebaconexionmysql.Models
 {
-    public partial class Usuario
+    public partial class Usuario: IdentityUser //para permitir hacer el login
     {
         public string CorreoElectronico { get; set; }
         public string Contraseña { get; set; }
